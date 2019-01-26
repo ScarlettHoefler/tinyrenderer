13805500db113b52beb75d2dd62cb64d247751dd
Scarlett Hoefler <scarlett.hoefler@gmail.com>
2019-01-26 02:13:29 -0800

    Refactored out effectively-duplicate drawing code between wide vs tall line drawing. To do this, we manually swap our axes, then swap them back before drawing

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -4,56 +4,36 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
-void drawWideLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    int width = std::abs(x0-x1);
-    int height = std::abs(y0-y1);
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    const bool tallerThanWide = std::abs(y0-y1) > std::abs(x0-x1);
+    
+    bool axesTransposed = false;
+    if (tallerThanWide) {
+        // We want to iterate over the Y coordinate instead of the X coordinate to make sure we fill out the entire line; otherwise we'll have gaps
+        // To avoid needing two nearly-identical code paths, instead we'll just swap our axes and then swap them back before drawing
+        std::swap(x0,y0);
+        std::swap(x1,y1);
+        axesTransposed = true;
+    }
     
     if (x0 > x1) {
+        // To simplify our looping logic, ensure we're drawing left-to-right by switching start and end coordinates
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
     
     for (int x = x0; x <= x1; ++x) {
-        float t = (float)(x-x0) / width;
+        float t = (float)(x-x0) / (x1-x0);
         
-        int targetY = y0 + (t * height);
-        std::cout << "x = " << x << ", y = " << targetY << "; t = " << t << std::endl;
+        int targetX = x;
+        int targetY = y0 + (t * (y1-y0));
         
-        image.set(x, targetY, color);
-    }
-}
-
-void drawTallLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    int width = std::abs(x0-x1);
-    int height = std::abs(y0-y1);
-    
-    if (y0 > y1) {
-        std::swap(x0,x1);
-        std::swap(y0,y1);
-    }
-    
-    for (int y = y0; y <= y1; ++y) {
-        float t = (float)(y-y0) / height;
+        if (axesTransposed) {
+            std::swap(targetX, targetY); // Flip the coords back to the correct axes before drawing
+        }
         
-        int targetX = x0 + (t * width);
-        std::cout << "x = " << targetX << ", y = " << y << "; t = " << t << std::endl;
-        
-        image.set(targetX, y, color);
+        image.set(targetX, targetY, color);
     }
-}
-
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    int width = std::abs(x0-x1);
-    int height = std::abs(y0-y1);
-    bool widerThanTall = width > height;
-    
-    if (widerThanTall) {
-        drawWideLine(x0, y0, x1, y1, image, color);
-    } else {
-        drawTallLine(x0, y0, x1, y1, image, color);
-    }
-    
-   
 }
 
 
