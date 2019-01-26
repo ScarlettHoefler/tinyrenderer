530d6f31c74da969cd1423e015773e7f1e695b64
Scarlett Hoefler <scarlett.hoefler@gmail.com>
2019-01-26 01:43:33 -0800

    WIP: line renderer (commit also includes adding TGAImage & some main.cpp start code from original tinyrenderer repo)

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -1,15 +1,39 @@
-//
-//  main.cpp
-//  tinyrenderer
-//
-//  Created by Scarlett Hoefler on 1/26/19.
-//  Copyright © 2019 Scarlett Hoefler. All rights reserved.
-//
-
+#include "tgaimage.h"
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    int width = std::abs(x0-x1);
+    int height = std::abs(y0-y1);
+    
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    
+    for (int x = x0; x <= x1; ++x) {
+        float t = (float)(x-x0) / width;
+        
+        int targetY = y0 + (t * height);
+        std::cout << "x = " << x << ", y = " << targetY << "; t = " << t << std::endl;
+        
+        image.set(x, targetY, color);
+    }
 }
+
+
+
+int main(int argc, char** argv) {
+	TGAImage image(100, 100, TGAImage::RGB);
+    
+    line(13, 20, 80, 40, image, white);
+    line(20, 13, 40, 80, image, red);
+    line(80, 40, 13, 20, image, red);
+    
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+	return 0;
+}
+
