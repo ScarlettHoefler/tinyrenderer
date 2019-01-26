1dcec7368372fca48b7f8f68eec072f0eaacb69f
Scarlett Hoefler <scarlett.hoefler@gmail.com>
2019-01-26 00:21:16 -0800

    Initial Commit

---
+++ tinyrenderer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tinyrenderer
+//
+//  Created by Scarlett Hoefler on 1/26/19.
+//  Copyright © 2019 Scarlett Hoefler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
