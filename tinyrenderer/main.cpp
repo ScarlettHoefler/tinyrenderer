3e2c2c6ce590940496b20e7bafa34e0943885b00
Scarlett Hoefler <scarlett.hoefler@gmail.com>
2019-02-03 22:50:42 -0800

    Now drawing our test model (human head) with random colors. Fixed issue where we could divide by zero during our triangle-drawing algorithm.

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -3,14 +3,15 @@
 #include <iostream>
 #include <cassert>
 #include <cmath>
+#include <random>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
 const TGAColor blue  = TGAColor(0,   0,   255, 255);
 
-const int ImageWidth = 200;
-const int ImageHeight = 200;
+const int ImageWidth = 800;
+const int ImageHeight = 800;
 
 template<typename T>
 struct Vector2 {
@@ -19,6 +20,11 @@ struct Vector2 {
         struct { T x, y; };
         T raw[2];
     };
+    
+    Vector2()
+    : Vector2(0,0)
+    {
+    }
     
     Vector2(T x, T y)
     : x(x), y(y)
@@ -71,12 +77,6 @@ void sweepLine(int leftX, int rightX, int y, TGAImage &image, TGAColor color) {
 }
 
 void triangle(Vector2i v0, Vector2i v1, Vector2i v2, TGAImage &image, TGAColor color) {
-    //* // Debug draw under
-    line(v0, v1, image, blue);
-    line(v1, v2, image, blue);
-    line(v2, v0, image, blue);
-    //*/
-    
     // Sort the 3 vectors from lowest to highest
     // 123 -> xx -> xx -> 123
     // 132 -> xx -> 123 -> 123
@@ -94,9 +94,14 @@ void triangle(Vector2i v0, Vector2i v1, Vector2i v2, TGAImage &image, TGAColor c
         std::swap(v0, v1);
     }
     
-    const int yDistV0toV1 = v1.y - v0.y;
-    const int yDistV0toV2 = v2.y - v0.y;
-    const int yDistV1toV2 = v2.y - v1.y;
+    int yDistV0toV1 = v1.y - v0.y;
+    int yDistV0toV2 = v2.y - v0.y;
+    int yDistV1toV2 = v2.y - v1.y;
+    
+    // HACK: Avoid divide-by-zero errors. ...this might skip drawing one edge of the triangle properly?
+    if (yDistV0toV1 == 0) yDistV0toV1 = 1;
+    if (yDistV0toV2 == 0) yDistV0toV2 = 1;
+    if (yDistV1toV2 == 0) yDistV1toV2 = 1;
     
     // Draw bottom half of triangle
     for (int curY = v0.y; curY <= v1.y; ++curY) {
@@ -128,15 +133,12 @@ void triangle(Vector2i v0, Vector2i v1, Vector2i v2, TGAImage &image, TGAColor c
         int rightX = (int)ceil(x2);
         sweepLine(leftX, rightX, curY, image, color);
     }
-    
-    /* // Debug draw over
-    line(v0, v1, image, blue);
-    line(v1, v2, image, blue);
-    line(v2, v0, image, blue);
-    //*/
 }
 
-void drawModel(const ObjModel& model, TGAImage &image) {
+void drawHeadWireframe(TGAImage &image) {
+    ObjModel model;
+    model.loadFromFile("obj/head.obj");
+    
     for (int faceIndex = 0; faceIndex < model.numFaces(); ++faceIndex) {
         ModelFace face = model.faceAtIndex(faceIndex);
         const int VerticesPerFace = 3;
@@ -152,22 +154,48 @@ void drawModel(const ObjModel& model, TGAImage &image) {
     }
 }
 
-void drawHead(TGAImage &image) {
-    ObjModel model;
-    model.loadFromFile("obj/head.obj");
-    drawModel(model, image);
-}
-
-
-int main(int argc, char** argv) {
-	TGAImage image(ImageWidth, ImageHeight, TGAImage::RGB);
-    
+void drawTestTriangles(TGAImage &image) {
     Vector2i t0[3] = {Vector2i(10, 70),   Vector2i(50, 160),  Vector2i(70, 80)};
     Vector2i t1[3] = {Vector2i(180, 50),  Vector2i(150, 1),   Vector2i(70, 180)};
     Vector2i t2[3] = {Vector2i(180, 150), Vector2i(120, 160), Vector2i(130, 180)};
     triangle(t0[0], t0[1], t0[2], image, red);
     triangle(t1[0], t1[1], t1[2], image, white);
     triangle(t2[0], t2[1], t2[2], image, green);
+}
+
+void drawHeadRandomColors(TGAImage &image) {
+    ObjModel model;
+    model.loadFromFile("obj/head.obj");
+    
+    std::random_device randomDevice;
+    std::default_random_engine randomEngine(randomDevice());
+    std::uniform_int_distribution<int> rgbUniformDist(0, 255);
+    
+    const size_t numFaces = model.numFaces();
+    for (int faceIndex = 0; faceIndex < numFaces; ++faceIndex) {
+        ModelFace face = model.faceAtIndex(faceIndex);
+        Vector2i screenCoords[3];
+        for (int iCoord = 0; iCoord < 3; ++iCoord) {
+            Vector3 worldCoords = model.vertexAtIndex(face[iCoord]);
+            int xPos = (worldCoords.x + 1.f) * ImageWidth / 2.f;
+            int yPos = (worldCoords.y + 1.f) * ImageHeight / 2.f;
+            screenCoords[iCoord] = Vector2i(xPos, yPos);
+        }
+        
+        int randomR = rgbUniformDist(randomEngine);
+        int randomG = rgbUniformDist(randomEngine);
+        int randomB = rgbUniformDist(randomEngine);
+        TGAColor randomColor(randomR, randomG, randomB, 255);
+        
+        triangle(screenCoords[0], screenCoords[1], screenCoords[2], image, randomColor);
+    }
+}
+
+
+int main(int argc, char** argv) {
+	TGAImage image(ImageWidth, ImageHeight, TGAImage::RGB);
+    
+    drawHeadRandomColors(image);
     
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
